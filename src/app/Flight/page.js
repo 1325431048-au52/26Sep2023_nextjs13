e646c3b70bb2a88988d8e65e06ff5185f1750958
1325431048-au52 <saurabhdixit5058@gmail.com>
2023-09-29 01:38:48 +0530

    day12nextjs

--- src/app/Flight/page.js
+++ src/app/Flight/page.js
@@ -1,18 +1,12 @@
-
-import './globals.css'
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react'
-export default function Home() {
-  return (
- 
 
- 
-        <section className='col-6 s_tbdr'>
-          <h1>The City</h1>
+export default function page() {
+  return (
+    <>
+    <section className='col-6 s_tbdr'>
+          <h1>The Flight</h1>
           <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap</p>
         </section>
-     
-  
-
+        </>
   )
 }
